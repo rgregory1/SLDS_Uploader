2254bf8f07f2fc3dc696c98ceae853e682729e32
rgregory1 <mrgregory1@gmail.com>
2020-11-30 14:11:57 -0500

    selenium test

--- hello.py
+++ hello.py
@@ -4,3 +4,16 @@ if platform.system() == "Darwin":
     print("mac")
 else:
     print("nada")
+
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    "/usr/lib/chromium-browser/chromedriver", chrome_options=options
+)
+
+print("Success")
