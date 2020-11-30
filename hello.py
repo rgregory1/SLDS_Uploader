f7fdf0529a9bfa0cc0f8f6f7e685a0e76980be96
rgregory1 <mrgregory1@gmail.com>
2020-11-30 14:14:23 -0500

    small change

--- hello.py
+++ hello.py
@@ -12,8 +12,6 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(
-    "/usr/lib/chromium-browser/chromedriver", chrome_options=options
-)
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=options)
 
 print("Success")
